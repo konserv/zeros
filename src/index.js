3c6547ad9d070662df40632b6e3a29f047756e28
konserv32 <konserv32@gmail.com>
2018-02-24 16:07:35 +0300

    yeahhhh!!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+var answer=0;
+for (i=5;i<=number;i=i+5) {
+	for (var x=5; i%x===0; x=x*5) {
+		answer++;
+	}	
+}
+
+return(answer);
 }
